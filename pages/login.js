7d9d98c72d063e755edcf9711a664ce1282d8aae
MOHAMED HAJJI <pytaek1@gmail.com>
2021-10-25 16:40:52 +0100

    lkhra i hope

--- pages/login.js
+++ pages/login.js
@@ -14,6 +14,10 @@ function Login() {
             <LoginContainer>
                 <Logo src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" />
                 <Button onClick={SignIn} variant="outlined">
+                    <img
+                        src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
+                        alt=""
+                    />{' '}
                     Sign In with Google
                 </Button>
             </LoginContainer>
@@ -28,13 +32,16 @@ const Container = styled.div`
     place-items: center;
     height: 100vh;
     background-color: whitesmoke;
+    background-image: url('https://cdn.wallpapersafari.com/91/2/DExBhK.jpg');
+    background-repeat: repeat;
+    background-size: contain;
 `;
 const LoginContainer = styled.div`
     display: flex;
     flex-direction: column;
     padding: 100px;
     align-items: center;
-    background: #fff;
+    background: transparent;
     border-radius: 5px;
     box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
 `;
@@ -44,4 +51,17 @@ const Logo = styled.img`
     margin-bottom: 50px;
 `;
 
-const Button = styled.div``;
+const Button = styled.div`
+    cursor: pointer;
+    background-color: #fff;
+    padding: 10px 15px;
+    border-radius: 5px;
+    font-weight: bold;
+    color: #333;
+    display: flex;
+    align-items: center;
+    box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.3);
+    img {
+        width: 40px;
+    }
+`;
