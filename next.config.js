245c5dea75a9b17a2c4af4e320cd0e2dd7eb73a5
MOHAMED HAJJI <pytaek1@gmail.com>
2021-10-25 12:20:49 +0100

    entire app1

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  reactStrictMode: true,
-}
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        // Important: return the modified config
+        return config;
+    },
+};
